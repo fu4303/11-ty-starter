fac9b8bb22548f51b3b79e681181361bf71e68e1
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-07-20 17:26:38 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -28,7 +28,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: 'src/',
-      output: 'dist',
+      output: '_layouts',
       includes: '_includes',
       layouts: '_layouts',
     },
